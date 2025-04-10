cfef426532aeb4b0edaab988d6e0485f504eb3a8
saivenkat.kotla <52619794+SaiVenkatKotla@users.noreply.github.com>
2025-04-10 16:05:53 +0530

    Executors and their implementation

---
+++ Practice/src/executors/PlayWithExecutorsAndRunnables.java
@@ -0,0 +1,30 @@
+package executors;
+
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
+public class PlayWithExecutorsAndRunnables {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Runnable task = () -> System.out.println("I'm in a runnable thread" + Thread.currentThread().getName());
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        IntStream.range(0, 10).forEach(p -> service.submit(task));
+
+        Callable<String> callableTask = () -> {
+            return "I'm in Callable Thread " + Thread.currentThread().getName();
+        };
+        service.shutdown();
+
+        ExecutorService callableService = Executors.newFixedThreadPool(4);
+        IntStream.range(0,10).forEach(p-> {
+            try {
+                System.out.println(callableService.submit(callableTask).get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        callableService.shutdown();
+
+    }
+
+}
